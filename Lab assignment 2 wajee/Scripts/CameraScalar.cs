f0f4d9bb936e1d99a5af85dda440881ac6a7a419
DESKTOP-TEE6DO0\ATech <wajeehawan7@gmail.com>
2021-04-18 01:07:56 +0500

    Lab assignment 2 uploaded

---
+++ Lab assignment 2 wajee/Scripts/CameraScalar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScalar : MonoBehaviour
+{
+    private Board board;
+    public float cameraOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        board = FindObjectOfType<Board>();
+        if (board != null)
+        {
+            RepositionCamera(board.width, board.height);
+        }
+    }
+
+    void RepositionCamera(float x, float y)
+    {
+        Vector2 tempPosition = new Vector3(x / 2, y / 2, cameraOffset);
+        transform.position = tempPosition;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
